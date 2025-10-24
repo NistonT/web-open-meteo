576a3767e64735a88fa74ce4780d120c7c67ae61
NistonN <nisprogrammer@gmail.com>
2025-10-24 14:15:50 +0300

    [XXXX] start project

---
+++ src/app/providers/ReduxProvider.tsx
@@ -0,0 +1,7 @@
+import { store } from "@/app/store";
+import { PropsWithChildren } from "react";
+import { Provider } from "react-redux";
+
+export const ReduxProvider = ({ children }: PropsWithChildren) => {
+  return <Provider store={store}>{children}</Provider>;
+};
